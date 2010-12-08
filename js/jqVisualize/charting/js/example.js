32dfb91cd4d3551b5b23375b734e9e93b93a370b
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-08 03:14:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpstorecart/tags/2.0.12@320293 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jqVisualize/charting/js/example.js
@@ -0,0 +1,7 @@
+// Run the script on DOM ready:
+$(function(){
+ $('table').visualize({type: 'pie', height: '300px', width: '620px'});
+	$('table').visualize({type: 'bar', width: '620px'});
+	$('table').visualize({type: 'area', width: '620px'});
+	$('table').visualize({type: 'line', width: '620px'});
+});
