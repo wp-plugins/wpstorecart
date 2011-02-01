98a7b8f1b46f48aea3ac59095a45be030680f3ec
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-01 04:01:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpstorecart/tags/2.1.6@339437 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/payment/paypal_success.php
@@ -0,0 +1,36 @@
+<?PHP
+
+// wpStoreCart, (c) 2010 wpStoreCart.com.  All rights reserved.
+
+//error_reporting(E_ALL);
+error_reporting(0);
+if (!function_exists('add_action'))
+{
+    require_once("../../../../../wp-config.php");
+}
+
+global $wpStoreCart, $current_user;
+get_currentuserinfo();
+$message = '';
+if (isset($wpStoreCart)) {
+	$devOptions = $wpStoreCart->getAdminOptions();
+
+         // In case any of our lines are larger than 70 characters, we should use wordwrap()
+        $message = wordwrap($wpStoreCart->makeEmailTxt($devOptions['emailonpurchase']) . $wpStoreCart->makeEmailTxt($devOptions['emailsig']), 70);
+
+        $headers = 'From: '.$current_user->user_email . "\r\n" .
+            'Reply-To: ' .$current_user->user_email. "\r\n" .
+            'X-Mailer: PHP/wpStoreCart v'.$wpstorecart_version;
+
+        // Send an email when purchase is submitted
+        mail($current_user->user_email, 'Thank you for your recent purchase', $message, $headers);
+
+
+        header ('HTTP/1.1 301 Moved Permanently');
+        if(strpos(get_permalink($devOptions['mainpage']),'?')===false) {
+            header ('Location: '.get_permalink($devOptions['mainpage']).'?wpsc=success');
+        } else {
+            header ('Location: '.get_permalink($devOptions['mainpage']).'&wpsc=success');
+        }
+}
+?>
