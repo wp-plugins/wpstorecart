d56bffc91a4cd812dca9b7a7e0e61213018f6755
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-13 05:06:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wpstorecart/tags/2.2.9@384280 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/wpsc-1.1/wpsc/wpsc-javascript.php
@@ -0,0 +1,10 @@
+<?php
+
+// wpsc library, v1.2 by wpStoreCart.com
+// Forked from jcart v1.1 http://conceptlogic.com/jcart/
+
+global $wpsc_error_reporting;
+if($wpsc_error_reporting==false) {
+    error_reporting(0);
+}
+?>
---
+++ wpStoreCartTheme/footer.php
@@ -0,0 +1,19 @@
+	</div><!-- Content End -->
+
+<div id="footer"><!-- Footer  Start -->
+            
+<div class="bg">&copy; <?PHP echo date('Y') .' '. get_bloginfo('name'); ?>. Design by wpStoreCart <a href="http://wpstorecart.com/">WP Shopping Cart</a></div>
+            
+</div><!-- Footer  End -->
+
+<div id="bottom"><!-- Bottom Space Start -->
+
+
+
+</div><!-- Bottom Space Start -->
+    
+
+</div><!-- Wrapper End -->
+
+</body>
+</html>
