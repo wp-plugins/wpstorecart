0fb16b6eb4a675f69aebc37e41d9f1fddc89fa1a
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-18 23:03:02 +0000

    * Added the ability to determine the order in which the quantity, pic, name, price, & remove are displayed on the checkout page & widget wpStoreCart > Settings > Display > Checkout Page > Field Order * Added the ability to switch between a new table based checkout or the original DIV based checkout wpStoreCart > Settings > Display > Checkout Page > Checkout XHTML Type * Icky.  Used a few inline styles on the registration/checkout process.  You can turn this off from wpStoreCart > Settings > Display > Checkout Page > Disable Inline Styles? set to Yes * jQuery UI Theme setting in wpStoreCart > Settings > Display > has been depreciated. If you want jQuery UI styles, then use a plugin dedicated to that. jQuery UI CSS markup is still present in wpStoreCart * Fixed a bug which caused the product title to be displayed on the main page, even when it was set to Off * Added new shortcode options orderby and ordertype, see the shortcode documentation for full details
    git-svn-id: https://plugins.svn.wordpress.org/wpstorecart/tags/2.3.14@411709 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/quetzalcoatl/quetzalcoatl.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Quetzalcoatl Ecommerce Library
+ *
+ * An open source ecommerce library, intended to be the backend store system for a number of ecommerce projects,
+ * including wpStoreCart, a Wordpress ecommerce plugin, as well as wpStoreCart for Drupal.  Licensed under the
+ * LGPL, it can be used in both commercial and open source software under the terms of the LGPL.
+ *
+ * @license LGPL
+ * @author Jeff Quindlen
+ * @version 0.0.1
+ */
+
+/**
+ * quetzalcoatl class
+ */
+class quetzalcoatl {
+    /**
+     * @var decimal Contains the total price
+     */
+    var $total = 0;
+
+    /**
+     * @var integer The number of items currently in the cart
+     */
+    var $itemcount = 0;
+
+    /**
+     * @var array An array of items added to the cart
+     */
+    var $items = array();
+
+    /**
+     * @var array An array of prices of the items currently in the cart
+     */
+    var $itemprices = array();
+
+    /**
+     * @var array An array of the quantities for each item in the cart
+     */
+    var $itemqtys = array();
+
+    /**
+     * @var array An array of the item names in the cart
+     */
+    var $itemname = array();
+
+    /**
+     * @var array An array of the flat rate shipping for each item in the cart
+     */
+    var $itemshipping = array();
+
+    /**
+     * @var array An array of tax costs for each item in the cart
+     */
+    var $itemtax = array();
+
+    /**
+     * @var array An array that contains the URL to the product page for each item in the cart
+     */
+    var $itemurl = array();
+
+    /**
+     * @var array An array that contains the URL to an image of each product in the cart
+     */
+    var $itemimg = array();
+
+    /**
+     * @var array An array that contains the discounts for each item in the cart
+     */
+    var $itemdiscount = array();
+
+    /**
+     * @var array An array that contains each item's category
+     */
+    var $itemcategory = array();
+
+}
+
+
+?>
