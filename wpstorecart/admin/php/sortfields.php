8a563ae3d3d9c79c90f710d00aab065f7d80f7aa
jquindlen <jquindlen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-14 04:04:13 +0000

    = 3.0.13 = * Added: a whole new set of product action hooks have been added.  Full details here: https://wpstorecart.com/documentation/wpstorecart-3/developers-api/extending-products/
    git-svn-id: https://plugins.svn.wordpress.org/wpstorecart/tags/3.0.13@638972 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpstorecart/admin/php/sortfields.php
@@ -0,0 +1,30 @@
+<?php
+if (!function_exists('add_action'))
+{
+    require_once("../../../../../../wp-config.php");
+}
+
+global $wpdb, $current_user;
+
+wp_get_current_user();
+if ( 0 == $current_user->ID ) {
+    // Not logged in.
+} else {
+
+    if (function_exists('current_user_can') && !current_user_can('manage_wpstorecart')) {
+            die(__('Unauthorized Access - wpStoreCart', 'wpstorecart'));
+    }
+
+    $table_name = $wpdb->prefix . "wpstorecart_meta";
+
+    // Grab the sort order
+    $ordernum = 1;
+    $sortorder = $_POST['requiredinfo'];
+    foreach ($sortorder as $sort) {
+        $updateSQL = "UPDATE  `{$table_name}` SET  `foreignkey` =  '{$ordernum}' WHERE  `primkey` ={$sort};";
+        $results = $wpdb->query($updateSQL);
+        $ordernum++;
+    }
+
+}
+?>
